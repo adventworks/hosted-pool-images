237e84db4e91b697526a816c69a0ba69e3c2b861
Mateo Escobedo <mat.esc@outlook.com>
2017-09-22 22:44:25 -0400

    check versions of all software

---
+++ vs2017-on-windows-2016/check-versions.ps1
@@ -0,0 +1,17 @@
+dotnet --version
+$PSVersionTable.PSVersion
+get-module -ListAvailable -name azurerm.compute
+get-module -ListAvailable -name azure
+az --version
+[System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files\Microsoft Service Fabric\bin\FabricHost.exe").FileVersion
+git --version
+git lfs version
+node --version
+npm --version
+docker version
+docker-compose -version
+java -version
+ant -version
+mvn -version
+cmake -version
+python --version
